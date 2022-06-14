506a951cdf91d26781ac5bd1e2f353c30f966d13
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-14 07:01:37 +0530

    init database

---
+++ app/src-tauri/src/database/init.rs
@@ -0,0 +1,16 @@
+use rusqlite::{Connection, Result};
+use std::path::Path;
+
+pub fn connect_database<P: AsRef<Path>>(database_path: P) -> Result<Connection> {
+  let conn = Connection::open(database_path)?;
+
+  conn.execute(
+    "CREATE TABLE IF NOT EXISTS patient (
+      id integer primary key,
+      data text,
+    )",
+    [],
+  )?;
+
+  Ok(conn)
+}
---
+++ app/src-tauri/src/database/mod.rs
@@ -0,0 +1,15 @@
+mod init;
+
+use rusqlite::Connection;
+
+pub use init::connect_database;
+
+pub fn init_database() -> Result<Connection, String> {
+  match connect_database("data.db") {
+    Ok(conn) => Ok(conn),
+    Err(err) => {
+      eprintln!("{}", err);
+      Err(String::from("Failed to connect database"))
+    }
+  }
+}
