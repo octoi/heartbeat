e7cffdd1d990c442475c33baeaed633e17da4b59
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-14 07:20:37 +0530

    chakra ui

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraWrapper } from './components/ChakraWrapper';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraWrapper>
+      <App />
+    </ChakraWrapper>
   </React.StrictMode>
-)
+);
