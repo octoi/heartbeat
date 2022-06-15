e31542ea7505654f07f63971e28d5b1c0d6a4f18
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-15 08:02:50 +0530

    header

---
+++ app/src/components/home/header/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Button, Flex, Input } from '@chakra-ui/react';
+import { IoMdPersonAdd } from 'react-icons/io';
+import { LogoDrawer } from './logoDrawer';
+import { Link } from 'react-router-dom';
+
+export const Header: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <Flex alignItems='center'>
+      <LogoDrawer />
+      <Input
+        placeholder='Search...'
+        type='text'
+        variant='filled'
+        size='lg'
+        mx={3}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <Link to='/edit'>
+        <Button size='lg' colorScheme='teal'>
+          <IoMdPersonAdd className='mr-2' />
+          New patient
+        </Button>
+      </Link>
+    </Flex>
+  );
+};
---
+++ app/src/components/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from './header';
+
+export const HomePageContent: React.FC = () => {
+  return (
+    <section>
+      <Header />
+    </section>
+  );
+};
