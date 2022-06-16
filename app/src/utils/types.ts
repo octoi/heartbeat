ee53460e780817ce7466177cb48fa8ad0ba53458
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-16 11:51:29 +0530

    patient types

--- app/src/utils/types.ts
+++ app/src/utils/types.ts
@@ -4,3 +4,58 @@ import { PropsWithChildren } from 'react';
 export type ReactComponent<Props = {}> = React.FC<PropsWithChildren<Props>>;
 
 export type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
+
+// According to PDF provided by a professional doctor
+export interface PatientBioData {
+  name?: string;
+  age?: string;
+  sex?: string;
+  address?: string;
+  height?: number;
+  weight?: number;
+  bmi?: number;
+  allergyToMedicine?: {
+    status?: boolean;
+    medicines?: string;
+  };
+  chiefComplaint?: string;
+  pastMedicalHistory?: string;
+  personalHistory?: string;
+  familyHistory?: string;
+  treatmentHistory?: string;
+}
+
+export interface PatientVitals {
+  pulseRate?: number;
+  bloodPressure?: {
+    systolic?: number;
+    diastolic?: number;
+  };
+  respiratoryRate?: number;
+  oxygenSaturation?: number;
+}
+
+export interface PatientSystemicExamination {
+  rs?: string;
+  cvs?: string;
+  git?: string;
+  musculoskeletal?: string;
+  provisionalOrFindDiagnosis?: string;
+  ddsIfAny?: string;
+}
+
+export interface PatientExamination {
+  generalExamination?: string;
+  vitals?: PatientVitals;
+  systemicExamination?: PatientSystemicExamination;
+}
+
+export interface PatientData {
+  bioData?: PatientBioData;
+  examination?: PatientExamination;
+}
+
+export interface Patient {
+  id: number;
+  data: PatientData;
+}
