293162335c11030a58c5b2aa12200797f02f2f82
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-17 07:22:04 +0530

    trying to save data

--- app/src/components/edit/form/index.tsx
+++ app/src/components/edit/form/index.tsx
@@ -1,14 +1,69 @@
-import { Button, SimpleGrid } from '@chakra-ui/react';
+import { Button, SimpleGrid, useToast } from '@chakra-ui/react';
+import { invoke } from '@tauri-apps/api/tauri';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { PatientData, SetState } from '../../../utils/types';
 import { BioData } from './bioData';
 import { ExaminationForm } from './examination';
 
-interface Props {}
+interface Props {
+  patientId?: number;
+}
 
-export const EditForm: React.FC<Props> = () => {
-  const [patientData, setPatientData] = useState({});
+export const EditForm: React.FC<Props> = ({ patientId }) => {
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const [patientData, setPatientData] = useState<PatientData>({});
   const [loading, setLoading] = useState(false);
+
+  const saveToDatabase = () => {
+    if (patientData?.bioData?.name?.trim().length === 0) {
+      toast({
+        title: 'Cant save patient details',
+        description: 'The `Name` field is required, you cant leave that empty',
+        duration: 3000,
+        isClosable: true,
+        position: 'top-right',
+        status: 'error',
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    if (patientId) {
+      return;
+    }
+
+    invoke('create_patient', {
+      data: JSON.stringify(patientData),
+    })
+      .then(() => {
+        navigate('/', { replace: false });
+        toast({
+          title: 'Saved patient data successfully',
+          description: `${patientData?.bioData?.name} is in your list now :)`,
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+          status: 'success',
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: err,
+          description: 'Please try again or report this as bug',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   return (
     <div className='mt-5'>
@@ -24,7 +79,12 @@ export const EditForm: React.FC<Props> = () => {
         loading={loading}
       />
       <SimpleGrid my={5} columns={{ sm: 1, lg: 2 }} gap={2}>
-        <Button colorScheme='blue' size='md' isLoading={loading}>
+        <Button
+          colorScheme='blue'
+          size='md'
+          isLoading={loading}
+          onClick={saveToDatabase}
+        >
           Save To Database
         </Button>
         <Button colorScheme='teal' size='md' disabled={loading}>
