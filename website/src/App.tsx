d914aa609e82569bc68db795d5f3e56d5991988f
Fadhil <fadhilsaheer@gmail.com>
2022-06-28 15:06:30 -0700

    footer

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,13 +3,15 @@ import { Container } from '@chakra-ui/react';
 import { Header } from './components/header';
 import { Landing } from './components/landing';
 import { About } from './components/about';
+import { Footer } from './components/footer';
 
 export const App: React.FC = () => {
   return (
-    <Container maxW='container.xl' mb={10}>
+    <Container maxW='container.xl'>
       <Header />
       <Landing />
       <About />
+      <Footer />
     </Container>
   );
 };
