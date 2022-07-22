16aa62c7ed9d5e148906e824edb49df6c70619a4
Fadhil <fadhilsaheer@gmail.com>
2022-07-21 21:50:14 -0700

    record form

---
+++ app/src/components/patient/records/RecordForm.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { PatientRecord, SetState } from '../../../utils/types';
+import { Advice } from './Advice';
+import { Examination } from './Examination';
+import { MedicalBioData } from './MedicalBioData';
+
+interface Props {
+  patientRecord: PatientRecord;
+  setPatientRecord: SetState<PatientRecord>;
+  loading: boolean;
+}
+
+export const RecordForm: React.FC<Props> = ({
+  patientRecord,
+  setPatientRecord,
+  loading,
+}) => {
+  return (
+    <div>
+      <MedicalBioData
+        patientRecord={patientRecord}
+        setPatientRecord={setPatientRecord}
+        loading={loading}
+      />
+      <Examination
+        patientRecord={patientRecord}
+        setPatientRecord={setPatientRecord}
+        loading={loading}
+      />
+      <Advice
+        patientRecord={patientRecord}
+        setPatientRecord={setPatientRecord}
+        loading={loading}
+      />
+    </div>
+  );
+};
